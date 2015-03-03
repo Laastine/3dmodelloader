6479abc29730deb9f706e9e5503b2a0d4b38b0cc
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2015-03-03 20:14:27 +0200

    Compiles and runs on VS2013

--- demo/d3dUtil.hpp
+++ demo/d3dUtil.hpp
@@ -68,7 +68,6 @@ D3DX10INLINE T Clamp(const T& x, const T& low, const T& high) {
     return x < low ? low : (x > high ? high : x);
 }
 
-const float INFINITY = FLT_MAX;
 const float PI       = 3.14159265358979323f;
 const float MATH_EPS = 0.0001f;
 
